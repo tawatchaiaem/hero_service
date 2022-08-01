8e699488bdf19bc8a26d1ca3e9b61b031e35009d
Tawatchai Hona <tawatchai.hona@ssup.co.th>
2022-08-01 17:01:27 +0700

    menu routing

--- lib/sceens/dashboard/dashboard_screen.dart
+++ lib/sceens/dashboard/dashboard_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hero_service_app/sceens/bottomnav/booking_screen.dart';
 import 'package:hero_service_app/sceens/bottomnav/home_sreen.dart';
+import 'package:hero_service_app/sceens/bottomnav/market_screnn.dart';
+import 'package:hero_service_app/sceens/bottomnav/setting_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   DashboardScreen({Key? key}) : super(key: key);
@@ -9,16 +12,50 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  int _currentIndex = 0;
+  String _title = 'Hero services';
+
+  final List<Widget> _children = [
+    HomeScreen(),
+    MarketScreen(),
+    BookingScreen(),
+    SettingScreen(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+
+      // change title follow tab
+      switch (index) {
+        case 0:
+          _title = 'services';
+          break;
+        case 1:
+          _title = 'market';
+          break;
+        case 2:
+          _title = 'booking';
+          break;
+        case 3:
+          _title = 'setting';
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hero Service'),
+        title: Center(child: Text('$_title')),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
         backgroundColor: Colors.red,
         type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.white,
+        selectedItemColor: Color.fromARGB(255, 248, 194, 194),
         unselectedItemColor: Colors.white,
         items: [
           BottomNavigationBarItem(
@@ -44,14 +81,14 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.library_books,
+              Icons.menu,
               color: Colors.white,
             ),
-            label: 'รายการจอง',
+            label: 'setting',
           )
         ],
       ),
-      body: HomeScreen(),
+      body: _children[_currentIndex],
     );
   }
 }
