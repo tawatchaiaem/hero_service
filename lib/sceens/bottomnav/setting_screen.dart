8e8aa0a8c1f53ccb63dbb435edc4c98ce07e90f1
programmertest9 <programmer.test.th@gmail>
2022-08-08 17:20:00 +0700

    setting page

--- lib/sceens/bottomnav/setting_screen.dart
+++ lib/sceens/bottomnav/setting_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,25 +14,71 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: ListView(
         children: [
-          Container(),
-          ElevatedButton(
-            onPressed: () async {
-              // สร้าง Object แบบ SharedPreferences
-              SharedPreferences sharedPreferences =
-                  await SharedPreferences.getInstance();
-
-              sharedPreferences.setInt('appStep', 3);
-
-              Navigator.pushReplacementNamed(context, '/lockscreen');
-            },
-            child: Text(
-              'Log out',
-              style: TextStyle(color: Colors.white),
+          Container(
+            width: double.infinity,
+            height: 180.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg_account.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-          )
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: CircleAvatar(
+                    radius: 50.0,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 46.0,
+                      backgroundImage: AssetImage('assets/images/profile.png'),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Tawatchai Hona',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('ข้อมูลผู้ใช้งาน'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text('เปลี่ยนรหัสผ่าน'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.language),
+            title: Text('เปลี่ยนภาษา'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('ติดต่อทีมงาน'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('ตั้งค่าใช้งาน'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('ออกจากระบบ'),
+            onTap: () {},
+          ),
         ],
       ),
     );
