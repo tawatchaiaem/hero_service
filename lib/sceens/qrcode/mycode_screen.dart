19904e7f745b48b21c7dc62fc90ee533149c37c1
tawatchaiaem <tawatchai.h1994@gmail.com>
2022-08-25 16:58:32 +0700

    qr code

--- lib/sceens/qrcode/mycode_screen.dart
+++ lib/sceens/qrcode/mycode_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class MyCodeScreen extends StatefulWidget {
   MyCodeScreen({Key? key}) : super(key: key);
@@ -10,8 +14,61 @@ class MyCodeScreen extends StatefulWidget {
 class _MyCodeScreenState extends State<MyCodeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('My code screen'),
+    final qrdata = 'https://www.google.co.th/contact-us';
+
+    final qrFutureBuilder = FutureBuilder(
+      future: _loadOverlayImage(),
+      builder: (ctx, snapshot) {
+        final size = 250.0;
+        if (!snapshot.hasData) {
+          return Container(width: size, height: size);
+        }
+        return CustomPaint(
+          size: Size.square(size),
+          painter: QrPainter(
+            data: qrdata,
+            version: QrVersions.auto,
+            // size: 320.0,
+            embeddedImage: snapshot.data?.data(),
+            embeddedImageStyle: QrEmbeddedImageStyle(
+              size: Size.square(60),
+            ),
+          ),
+        );
+      },
     );
+
+    return Material(
+      color: Colors.white,
+      child: SafeArea(
+        top: true,
+        bottom: true,
+        child: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Center(
+                    child: qrFutureBuilder,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40)
+                    .copyWith(bottom: 40),
+                child: Text('My qrcode data'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<ui.Image> _loadOverlayImage() async {
+    final completer = Completer<ui.Image>();
+    final byteData = await rootBundle.load('assets/images/logo_qr.png');
+    ui.decodeImageFromList(byteData.buffer.asUint8List(), completer.complete);
+    return completer.future;
   }
 }
